b0a2a190efbd8b2e54630c65f37782317ed7c026
Afsal Khan <afsalkhanmdt@gmail.com>
2023-03-22 14:42:21 +0530

    ok

---
+++ webDist/_next/static/chunks/app/layout-b12487d95e7f4a83.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{2110:function(n,e,u){Promise.resolve().then(u.t.bind(u,4026,23))},4026:function(){}},function(n){n.O(0,[916,744],function(){return n(n.s=2110)}),_N_E=n.O()}]);
